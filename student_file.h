8ab104805d28268a4a03715c28b2e794f186a2e2
Jose Carlos Machaca Vera <130808338+xPorotin9@users.noreply.github.com>
2023-11-03 18:56:13 -0600

    Implementacion en archivo .h

--- student_file.h
+++ student_file.h
@@ -1,1 +1,36 @@
-
+#include <vector>
+#include <queue>
+using namespace std;
+vector<int> Torneo(vector<int> habilidades, int N, int K) {
+    queue<int> cola;
+    for (size_t i = 2; i < habilidades.size(); ++i) {
+        cola.push(habilidades[i]);
+    }
+    int wins = 0; int actual = habilidades[0];int retador = habilidades[1];
+    vector<int> result(2);
+    for (int juego = 1; juego <= K; ++juego) {
+        if (wins > retador) 
+        {
+            wins++;
+            result = {retador, wins};
+            cola.push(retador);
+        } 
+        else 
+        {
+            wins = 1;
+            result = {wins, retador};
+            cola.push(wins);
+            wins = retador;
+        }
+        if (wins == N) 
+        {
+            cola.push(wins);
+            wins = cola.front();
+            cola.pop();
+            wins = 0;
+        }
+        retador = cola.front(); 
+        cola.pop();
+    }
+    return result;
+}
