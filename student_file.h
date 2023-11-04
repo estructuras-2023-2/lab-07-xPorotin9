ea7531f5145748de1cff772fea8c59076c7c4a1b
Jose Carlos Machaca Vera <130808338+xPorotin9@users.noreply.github.com>
2023-11-03 21:02:37 -0600

    Update student_file.h

--- student_file.h
+++ student_file.h
@@ -4,31 +4,24 @@ using namespace std;
 vector<int> Torneo(vector<int> habilidades, int N, int K) {
     queue<int> cola;
     for (size_t i = 2; i < habilidades.size(); ++i) {
-        cola.push(habilidades[i]);
-    }
-    int wins = 0; int actual = habilidades[0];int retador = habilidades[1];
+        cola.push(habilidades[i]);}
+    int Ganados = 0; int set = habilidades[0];int retador = habilidades[1];
     vector<int> result(2);
-    for (int juego = 1; juego <= K; ++juego) {
-        if (wins > retador) 
-        {
-            wins++;
-            result = {retador, wins};
+    for (int game = 1; game <= K; ++game){
+        if (set > retador){
+            Ganados++;
+            result = {retador, set};
             cola.push(retador);
-        } 
-        else 
-        {
-            wins = 1;
-            result = {wins, retador};
-            cola.push(wins);
-            wins = retador;
-        }
-        if (wins == N) 
-        {
-            cola.push(wins);
-            wins = cola.front();
+        }else{
+            Ganados = 1;
+            result = {set, retador};
+            cola.push(set);
+            set = retador;}
+        if (Ganados == N){
+            cola.push(set);
+            set = cola.front();
             cola.pop();
-            wins = 0;
-        }
+            Ganados = 0;}
         retador = cola.front(); 
         cola.pop();
     }
